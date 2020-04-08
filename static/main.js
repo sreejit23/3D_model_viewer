6144bb57ebcb76140b527ee71980f72042757890
sreejit23 <sreejit.pillai23@gmail.com>
2020-04-08 19:26:04 +0530

    first commit

---
+++ static/main.js
@@ -0,0 +1,119 @@
+//Model
+
+var theModel;
+const MODEL_PATH =  "models/black.glb";
+
+const BACKGROUND_COLOR = 0xf1f1f1;
+// Init the scene
+const scene = new THREE.Scene();
+// Set background
+scene.background = new THREE.Color(BACKGROUND_COLOR );
+scene.fog = new THREE.Fog(BACKGROUND_COLOR, 20, 100);
+
+
+const canvas = document.querySelector('#c');
+
+// Init the renderer
+const renderer = new THREE.WebGLRenderer({canvas, antialias: true});
+renderer.shadowMap.enabled = true;
+renderer.setPixelRatio(window.devicePixelRatio); 
+document.body.appendChild(renderer.domElement);
+
+var cameraFar = 5;
+
+// Add a camera
+var camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = cameraFar;
+camera.position.x = 0;
+
+
+// Init the object loader
+var loader = new THREE.GLTFLoader();
+
+loader.load(MODEL_PATH, function(gltf) {
+  theModel = gltf.scene;
+  theModel.traverse((o) => {
+     if (o.isMesh) {
+       o.castShadow = true;
+       o.receiveShadow = true;
+     }
+   });
+// Set the models initial scale   
+  theModel.scale.set(2,2,2);
+
+  //theModel.rotation.y = Math.PI;
+
+  // Offset the y position a bit
+  theModel.position.y = -0.5;
+
+  // Add the model to the scene
+  scene.add(theModel);
+
+}, undefined, function(error) {
+  console.error(error)
+});
+
+// Add lights
+var hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 0.61 );
+    hemiLight.position.set( 0, 50, 0 );
+// Add hemisphere light to scene   
+scene.add( hemiLight );
+
+var dirLight = new THREE.DirectionalLight( 0xffffff, 0.54 );
+    dirLight.position.set( -8, 12, 8 );
+    dirLight.castShadow = true;
+    dirLight.shadow.mapSize = new THREE.Vector2(1024, 1024);
+// Add directional Light to scene    
+    scene.add( dirLight );
+
+// Floor
+var floorGeometry = new THREE.PlaneGeometry(5000, 5000, 1, 1);
+var floorMaterial = new THREE.MeshPhongMaterial({
+  color: 0xeeeeee,
+  shininess: 0
+});
+
+var floor = new THREE.Mesh(floorGeometry, floorMaterial);
+floor.rotation.x = -0.5 * Math.PI;
+floor.receiveShadow = true;
+floor.position.y = -1;
+scene.add(floor);
+
+function resizeRendererToDisplaySize(renderer) {
+  const canvas = renderer.domElement;
+  var width = window.innerWidth;
+  var height = window.innerHeight;
+  var canvasPixelWidth = canvas.width / window.devicePixelRatio;
+  var canvasPixelHeight = canvas.height / window.devicePixelRatio;
+
+  const needResize = canvasPixelWidth !== width || canvasPixelHeight !== height;
+  if (needResize) {
+    
+    renderer.setSize(width, height, false);
+  }
+  return needResize;
+}
+
+// Add controls
+var controls = new THREE.OrbitControls( camera, renderer.domElement );
+controls.maxPolarAngle = Math.PI / 2;
+controls.minPolarAngle = Math.PI / 3;
+controls.enableDamping = true;
+controls.enablePan = false;
+controls.dampingFactor = 0.1;
+controls.autoRotate = false; // Toggle this if you'd like the chair to automatically rotate
+controls.autoRotateSpeed = 0.2; // 30
+
+function animate() {
+  controls.update();
+  renderer.render(scene, camera);
+  requestAnimationFrame(animate);
+  
+  if (resizeRendererToDisplaySize(renderer)) {
+    const canvas = renderer.domElement;
+    camera.aspect = canvas.clientWidth / canvas.clientHeight;
+    camera.updateProjectionMatrix();
+  }
+}
+animate();
+
